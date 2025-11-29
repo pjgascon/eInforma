605661aefa44859e306de537e84f1f8983aae41e
Pedro <pedrojose.gascon@liberi.es>
2025-11-29 10:34:07 +0100

    Se añade obtencion del numero de telefono de paginas amarillas

---
+++ telefono.js
@@ -0,0 +1,65 @@
+import mysql from 'mysql2/promise';
+import puppeteer from 'puppeteer-extra';
+import StealthPlugin from 'puppeteer-extra-plugin-stealth';
+import { JSDOM } from 'jsdom';
+puppeteer.use(StealthPlugin());
+
+async function obtenerHTML(nombre, poblacion) {
+    const browser = await puppeteer.launch({
+        executablePath: '/usr/bin/google-chrome',
+        headless: true,
+        args: ['--window-size=800,600',
+            '--window-position=-1000,0',
+            '--disable-backgrounding-occluded-windows'],
+        defaultViewport: null
+    });
+    const page = await browser.newPage();
+
+    await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36');
+    await page.goto("https://www.paginasamarillas.es/");
+    await page.type('#whatInput', nombre);
+    await page.type('#where', poblacion);
+    await page.keyboard.press('Enter');
+    await page.keyboard.press('Enter');
+
+    await page.waitForNavigation({ waitUntil: 'networkidle0' });
+    const pois = await page.evaluate(() => {
+        const script = [...document.scripts].find(s =>
+            s.textContent.includes('PAOL.mapaPois.addPois')
+        );
+        if (!script) return [];
+
+        const regex = /PAOL\.mapaPois\.addPois\(\s*(\[[\s\S]*?\])\s*\)/;
+        const match = script.textContent.match(regex);
+        if (!match) return [];
+
+        const arrayLiteral = match[1]; // ej: [{name:"Minsaez", ...}]
+
+        // Esto se ejecuta en el contexto del navegador
+        const pois = eval(arrayLiteral);
+        return pois;
+    });
+    const poi = pois[0];
+
+    const name = poi.name;
+    const address = poi.address;
+    const phone = poi.phone;
+    const lat = poi.lat;
+    const lng = poi.lng;
+
+    console.log("Nombre:", pois[0].name);
+    console.log("Dirección:", pois[0].address);
+    console.log("Teléfono:", pois[0].phone);
+    console.log("Latitud:", pois[0].lat);
+    console.log("Longitud:", pois[0].lng);
+    // Si solo quieres el primero:
+    // console.log('Primer POI:', pois[0]);
+    await browser.close();
+
+    //return pageHTML;
+}
+const args = process.argv.slice(2);
+const nombre = args[0];
+const poblacion = args[1];
+
+await obtenerHTML(nombre, poblacion);
